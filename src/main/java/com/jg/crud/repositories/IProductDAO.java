099b26688371f4c3bf65b16fbff9ae48b2ceb4b8
Jhudyt Gaona <jhudyt.gaona.est@tecazuay.edu.ec>
2023-07-07 10:24:48 -0500

    Initial

---
+++ src/main/java/com/jg/crud/repositories/IProductDAO.java
@@ -0,0 +1,11 @@
+package com.jg.crud.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jg.crud.models.ProductDTO;
+
+@Repository
+public interface IProductDAO extends MongoRepository<ProductDTO, String>{
+
+}
